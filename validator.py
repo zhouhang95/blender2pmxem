af91cb4b7156e6c7451aacea08878d5c936265e9
zhouhang95 <765229842@qq.com>
2021-01-29 15:37:18 +0800

    more info when name conflict

--- validator.py
+++ validator.py
@@ -8,34 +8,39 @@
 from .pmx import pmx
 from typing import List
 
+def check_unique(name_list: List[str], category: str) -> List[str]:
+    result = []
+    name_set = set()
+    for i in range(len(name_list)):
+        name = name_list[i]
+        if name not in name_set:
+            name_set.add(name)
+        else:
+            result.append('{} name {}:{} must be unique in PMX.'.format(category, i, name))
+    return result
 
 def validate_pmx(pmx_data: pmx.Model, use_ja_name: bool) -> List[str]:
     result = []
 
     if use_ja_name:
         morph_name_list = [morph.Name for morph in pmx_data.Morphs]
-        if len(morph_name_list) != len(set(morph_name_list)):
-            result.append('Morph Japanese name must be unique in PMX.')
+        result.extend(check_unique(morph_name_list, 'Morph Japanese'))
 
         bone_name_list = [bone.Name for bone in pmx_data.Bones]
-        if len(bone_name_list) != len(set(bone_name_list)):
-            result.append('Bone Japanese name must be unique in PMX.')
+        result.extend(check_unique(bone_name_list, 'Bone Japanese'))
     else:
         morph_name_list = [morph.Name_E for morph in pmx_data.Morphs]
-        if len(morph_name_list) != len(set(morph_name_list)):
-            result.append('Morph English name must be unique in PMX.')
+        result.extend(check_unique(morph_name_list, 'Morph English'))
 
         bone_name_list = [bone.Name_E for bone in pmx_data.Bones]
-        if len(bone_name_list) != len(set(bone_name_list)):
-            result.append('Bone English name must be unique in PMX.')
+        result.extend(check_unique(bone_name_list, 'Bone English')) 
+
 
     rigid_name_list = [rigid.Name for rigid in pmx_data.Rigids]
-    if len(rigid_name_list) != len(set(rigid_name_list)):
-        result.append('Rigid name must be unique in PMX.')
+    result.extend(check_unique(rigid_name_list, 'Rigid English')) 
 
     joint_name_list = [joint.Name for joint in pmx_data.Joints]
-    if len(joint_name_list) != len(set(joint_name_list)):
-        result.append('Joint name must be unique in PMX.')
+    result.extend(check_unique(joint_name_list, 'Joint English')) 
 
     return result
 
